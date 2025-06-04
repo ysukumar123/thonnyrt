9cfba176a777217ae4ddc312b905afbafd0be839
ysukumar123 <yadevs723@deltalearns.ca>
2025-06-04 10:35:15 -0700

    intitoinal
    fdgdfg

---
+++ main.py
@@ -0,0 +1,26 @@
+# Quiz Game
+# This will ask the user some questions and check their answers
+# Points will be given for each correct answer
+# Points will be taken away for wrong answers
+
+# Welcome user to the game
+print("Welcome to our Quix Game!")
+
+# Ask user for their names
+name=input("What is your name")
+
+print(f"Hey {name}, Thats a nice {name}!, maybe i'll name my kids after you.")
+
+# Ask the first quiz question
+
+# Check the answer
+
+# Assing points
+print("1. What is Kendrick's latest Album?")
+
+# ask the second question
+#check answer
+#assingn points
+
+# show the end score
+# say thanks for playing
